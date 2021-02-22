213b267b74c6ecd5db4fd33741cdfb689076f170
cchauve-canexia <cchauve@canexiahealth.com>
2021-02-22 19:15:01 +0000

    more results

---
+++ results/analyze_errors.py
@@ -0,0 +1,86 @@
+"""
+Analyze FP and FN for the optimal thresholds
+"""
+from collections import defaultdict
+import sys
+import numpy as np
+import pandas as pd
+
+GRID = [
+    'VAF_NG_0.0_0.5_0.0_4.0', 'VAF_NG_0.0_0.5_4.0_8.0',
+    'VAF_NG_0.0_0.5_8.0_16.0', 'VAF_NG_0.0_0.5_16.0_32.0',
+    'VAF_NG_0.0_0.5_32.0_100.0',
+    'VAF_NG_0.5_1.0_0.0_4.0', 'VAF_NG_0.5_1.0_4.0_8.0',
+    'VAF_NG_0.5_1.0_8.0_16.0', 'VAF_NG_0.5_1.0_16.0_32.0',
+    'VAF_NG_0.5_1.0_32.0_100.0',
+    'VAF_NG_1.0_5.0_0.0_4.0', 'VAF_NG_1.0_5.0_4.0_8.0',
+    'VAF_NG_1.0_5.0_8.0_16.0', 'VAF_NG_1.0_5.0_16.0_32.0',
+    'VAF_NG_1.0_5.0_32.0_100.0'
+]
+
+PREF = 'v4MiSeq_commercial_samples_expected_indels_ExpectedVAFs_MissingSeraSeq'
+SUFF = 'optimal_thresholds_with_blacklist'
+FEATURES = ['vaf', 'comp.', 'supp.', 'overlap', 'ctrl']
+ERROR_TYPES = ['FP', 'FN_u', 'FN_d']
+
+header = '\t'.join(
+    ['freq.', 'chr', 'pos', 'ref', 'alt'] +
+    [f"avg_{x}\tmax_{x}" for x in FEATURES]
+)
+FEATURES_VAL = {
+    error_type: {feature: defaultdict(list) for feature in FEATURES}
+    for error_type in ERROR_TYPES
+}
+FEATURES_AVG = {
+    error_type: {feature: defaultdict(float) for feature in FEATURES}
+    for error_type in ERROR_TYPES
+}
+FEATURES_MAX = {
+    error_type: {feature: defaultdict(float) for feature in FEATURES}
+    for error_type in ERROR_TYPES
+}
+VARIANTS_COUNT = {error_type: defaultdict(int) for error_type in ERROR_TYPES}
+OUT_FILES = {
+    error_type: f"{PREF}_{SUFF}_{error_type}.txt" for error_type in ERROR_TYPES
+}
+
+
+for setting in GRID:
+    in_file = '_'.join([PREF, setting, SUFF, 'errors.txt'])
+    in_df = pd.read_csv(in_file, dtype=str, sep='\t')
+    for _, row in in_df.iterrows():
+        v_key = '_'.join([
+            str(x) for x in [row['chr'], row['pos'], row['ref'], row['alt']]
+        ])
+        error_type = row['w_comp']
+        # Bug in output: LLOD used as index, error type not in header
+        VARIANTS_COUNT[error_type][v_key] += 1
+        for feature in FEATURES:
+            FEATURES_VAL[error_type][feature][v_key].append(float(row[feature]))
+
+for error_type in ERROR_TYPES:
+    out_file = open(OUT_FILES[error_type], 'w')
+    out_file.write(header)
+    VARIANTS_KEYS = list(VARIANTS_COUNT[error_type])
+    VARIANTS_KEYS.sort(
+        key=lambda x: VARIANTS_COUNT[error_type][x],
+        reverse=True
+    )
+    for v_key in VARIANTS_KEYS:
+        for feature in FEATURES:
+            FEATURES_AVG[error_type][feature][v_key] = (
+                round(np.mean(FEATURES_VAL[error_type][feature][v_key]), 3)
+            )
+            FEATURES_MAX[error_type][feature][v_key] = (
+                round(np.max(FEATURES_VAL[error_type][feature][v_key]), 3)
+            )
+        out_str_frequency = [str(VARIANTS_COUNT[error_type][v_key])]
+        out_str_variant = v_key.split('_')
+        out_str_features = [
+            f"{FEATURES_AVG[error_type][feature][v_key]}\t{FEATURES_MAX[error_type][feature][v_key]}"
+            for feature in FEATURES
+        ]
+        out_str = '\t'.join(
+            out_str_frequency + out_str_variant + out_str_features)
+        out_file.write(f"\n{out_str}")
+    out_file.close()
---
+++ results/filter_results.py
@@ -0,0 +1,65 @@
+"""
+Extract the best results from the grid parameters exploration
+"""
+import collections
+import sys
+import numpy as np
+import pandas as pd
+
+EXT = sys.argv[1]
+FACTOR = float(sys.argv[2])
+COL1 = sys.argv[3]
+COL2 = sys.argv[4]
+MIN_FREQUENCY = int(sys.argv[5])
+
+GRID = [
+    'VAF_NG_0.0_0.5_0.0_4.0', 'VAF_NG_0.0_0.5_4.0_8.0',
+    'VAF_NG_0.0_0.5_8.0_16.0', 'VAF_NG_0.0_0.5_16.0_32.0',
+    'VAF_NG_0.0_0.5_32.0_100.0',
+    'VAF_NG_0.5_1.0_0.0_4.0', 'VAF_NG_0.5_1.0_4.0_8.0',
+    'VAF_NG_0.5_1.0_8.0_16.0', 'VAF_NG_0.5_1.0_16.0_32.0',
+    'VAF_NG_0.5_1.0_32.0_100.0',
+    'VAF_NG_1.0_5.0_0.0_4.0', 'VAF_NG_1.0_5.0_4.0_8.0',
+    'VAF_NG_1.0_5.0_8.0_16.0', 'VAF_NG_1.0_5.0_16.0_32.0',
+    'VAF_NG_1.0_5.0_32.0_100.0'
+]
+
+PREF = 'v4MiSeq_commercial_samples_expected_indels_ExpectedVAFs_MissingSeraSeq'
+
+header = ['LLOD', 'score']
+header += ['w_comp', 'TP', 'FP', 'TN', 'FN_u', 'FN_d']
+header += ['sens.', 'spec.', 'acc.', 'prec.', 'recall', 'F1', 'FDR']
+
+COMBINATIONS = []
+for setting in GRID:
+    setting_split = setting.replace('VAF_NG_', '').split('_')
+    vaf_min = setting_split[0]
+    vaf_max = setting_split[1]
+    ng_min = setting_split[2]
+    ng_max = setting_split[3].replace('100.0', '64.0')
+    info1 = [vaf_min, vaf_max, ng_min, ng_max]
+    in_file = '_'.join([PREF, setting, EXT, 'out.tsv'])
+    in_df = pd.read_csv(in_file, dtype=str, sep='\t')
+    in_df_columns = list(in_df.columns)
+    in_df[COL1] = in_df[COL1].astype(np.float64)
+    in_df[COL2] = in_df[COL2].astype(np.float64)
+    COL1_max = in_df[COL1].max()
+    COL2_max = in_df[COL2].max()
+    print(f"\n### VAF=({vaf_min},{vaf_max}] DNA=({ng_min},{ng_max}]\n#### FILE={in_file}")
+    print('```')
+    print('\t'.join(header))
+    for _, row in in_df.iterrows():
+        if (row[COL1] >= FACTOR * COL1_max) and (row[COL2] >= FACTOR * COL2_max):
+            info = info1 + [row[c] for c in in_df_columns]
+            print('\t'.join([str(x) for x in info]))
+            COMBINATIONS.append((row['score'], row['w_comp']))
+    print('```')
+    
+print('\n### Combinations frequencies\n```\nscore\tw_comp\tfrequency')
+FREQUENCIES = collections.Counter(COMBINATIONS)
+FREQUENCIES_KEYS = list(FREQUENCIES.keys())
+FREQUENCIES_KEYS.sort(key=lambda c: FREQUENCIES[c])
+for key in FREQUENCIES_KEYS:
+    if FREQUENCIES[key] >= MIN_FREQUENCY:
+        print(f"{key[0]}\t{key[1]}\t{FREQUENCIES[key]}")
+print('```')
