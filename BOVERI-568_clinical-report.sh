bcf4a5870851dcb2fcde7db3292d93671dbdf83d
cchauve-canexia <cchauve@canexiahealth.com>
2021-03-02 17:35:44 +0000

    removing old results

--- BOVERI-568_clinical-report.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# python bin/extract_files.py data/BOVERI-568_commercial.csv
-# python bin/dump_variants.py data/BOVERI-568_commercial.csv -m CG001v5.1_Amplicon_Manifest_Panel5.1.12_20200911.tsv
-python bin/analyze_variants_clinical.py data/v51NextSeq_clinical_samples_expected_indels_NO_WT.tsv BOVERI-568_clinical-report.yaml
-
